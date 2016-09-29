541c7dd848f06ae01d620a0fae7932721e7bda9b
Troy <tward4@tulane.edu>
2016-09-29 15:34:03 -0400

    Adding AddClick and Long Click

--- app/src/main/java/com/troyward/contactsandroid/MainActivity.java
+++ app/src/main/java/com/troyward/contactsandroid/MainActivity.java
@@ -3,12 +3,13 @@ package com.troyward.contactsandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
     ListView list;
     EditText editName;
     EditText editPhone;
@@ -28,15 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         contacts = new ArrayAdapter<Contact>(this,android.R.layout.simple_list_item_1);
         list.setAdapter(contacts);
+
+        addButton.setOnClickListener(this);
+        list.setOnItemLongClickListener(this);
     }
 
-//    @Override
-//    public void onClick(View view) {
-//        String t = editName.getText().toString();
-//        if(!t.isEmpty()) {
-//            ToDoItem item = new ToDoItem(t, false);
-//            items.add(item);
-//            text.setText("");
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        String t = editName.getText().toString();
+        String t2 = editPhone.getText().toString();
+        if(!t.isEmpty() || !t2.isEmpty()) {
+            Contact contact = new Contact(t,t2);
+            contacts.add(contact);
+            editName.setText("");
+            editPhone.setText("");
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Contact contact = contacts.getItem(position);
+        contacts.remove(contact);
+        return true;
+    }
 }
